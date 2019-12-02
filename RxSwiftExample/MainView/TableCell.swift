4de8f36d469c1b6ad530e2fcd45c0708624be6d6
Faruk Amin <aminfaruk.fa@gmail.com>
2019-12-02 19:48:44 +0700

    initial commit

---
+++ RxSwiftExample/MainView/TableCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableCell.swift
+//  RxSwiftExample
+//
+//  Created by Amin faruq on 02/12/19.
+//  Copyright © 2019 Amin faruq. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RxSwiftExample/ViewModel/PostVM.swift
@@ -0,0 +1,9 @@
+//
+//  PostVM.swift
+//  RxSwiftExample
+//
+//  Created by Amin faruq on 02/12/19.
+//  Copyright © 2019 Amin faruq. All rights reserved.
+//
+
+import Foundation
